d96cc8065ad23d56e4fe2bcae681bb596980f009
Hamdi Kahloun <hamdi.kahloun@vegagroup.com.tn>
2019-11-02 13:07:26 +0100

    Add Fragment & click effect

--- app/src/main/java/com/veganet/developerjobportal/MainActivity.java
+++ app/src/main/java/com/veganet/developerjobportal/MainActivity.java
@@ -1,12 +1,23 @@
 package com.veganet.developerjobportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.veganet.developerjobportal.fargments.HomeFragment;
+import com.veganet.developerjobportal.fargments.ProfileFragment;
+import com.veganet.developerjobportal.fargments.SearchFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(flags);
         }
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        setCurrentFragment(new HomeFragment());
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_home :
+                setCurrentFragment(new HomeFragment());
+                return true;
+            case R.id.action_search :
+                setCurrentFragment(new SearchFragment());
+                return true;
+            case R.id.action_profile :
+                setCurrentFragment(new ProfileFragment());
+                return true;
+        }
+        return false;
     }
 }
